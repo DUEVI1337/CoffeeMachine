20fc110bfa74aca17a48deb84cd08e916d6cd78c
Егор Дуванов <duvanov.zhora@gmail.com>
2022-06-24 09:01:30 +0700

    add xml-doc for mapper

--- src/CoffeeMachine.Application/Mappers/Mapper.cs
+++ src/CoffeeMachine.Application/Mappers/Mapper.cs
@@ -11,23 +11,23 @@ using CoffeeMachine.Domain.DTO;
 namespace CoffeeMachine.Application.Mappers
 {
     /// <summary>
-    /// 
+    /// map entities to DTO
     /// </summary>
     public class Mapper
     {
         /// <summary>
-        /// 
+        /// map 'Coffee' to 'CoffeeDto'
         /// </summary>
         /// <param name="coffee"></param>
-        /// <returns></returns> 
+        /// <returns><see cref="CoffeeDto"/></returns> 
         public static CoffeeDto CoffeeMapper(Coffee coffee) =>
             new () { CoffeeId = coffee.CoffeeId.ToString(), CoffeeName = coffee.Name, CoffeePrice = coffee.Price };
 
         /// <summary>
-        /// 
+        /// map list 'Coffee' to list 'CoffeeDto'
         /// </summary>
         /// <param name="coffees"></param>
-        /// <returns></returns>
+        /// <returns><see cref="List{T}"/> where T <see cref="CoffeeDto"/></returns>
         public static List<CoffeeDto> CoffeeListMapper(List<Coffee> coffees)
         {
             List<CoffeeDto> coffeesDto = new List<CoffeeDto>();
